6fe3cc43db3886177349494e7323346f0f73730a
Omkar Kadam <omkarakadam2408@gmail.com>
2026-02-20 10:56:53 +0530

    Update Preemptive SJF SRTF.java

--- Preemptive SJF SRTF.java
+++ Preemptive SJF SRTF.java
@@ -1,85 +1,89 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class SJF_Preemptive {
-
-    static void findAvgTime(int bt[], int at[]) {
-
-        int n = bt.length;
-
-        int[] rt = new int[n]; // remaining time
-        int[] wt = new int[n];
-        int[] tat = new int[n];
-        int[] ct = new int[n];
-
-        for (int i = 0; i < n; i++)
-            rt[i] = bt[i];
-
-        int complete = 0, currentTime = 0;
-        int min = Integer.MAX_VALUE;
-        int shortest = -1;
-        boolean found = false;
-
-        int totalWT = 0, totalTAT = 0;
-
-        while (complete != n) {
-
-            // Find process with minimum remaining time
-            for (int i = 0; i < n; i++) {
-                if (at[i] <= currentTime && rt[i] > 0 && rt[i] < min) {
-                    min = rt[i];
-                    shortest = i;
-                    found = true;
-                }
-            }
-
-            if (!found) {
-                currentTime++;
-                continue;
-            }
-
-            rt[shortest]--;
-            min = rt[shortest];
-
-            if (min == 0)
-                min = Integer.MAX_VALUE;
-
-            // If process completes
-            if (rt[shortest] == 0) {
-                complete++;
-                found = false;
-
-                ct[shortest] = currentTime + 1;
-                wt[shortest] = ct[shortest] - bt[shortest] - at[shortest];
-
-                if (wt[shortest] < 0)
-                    wt[shortest] = 0;
-
-                totalWT += wt[shortest];
-            }
-
-            currentTime++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            tat[i] = bt[i] + wt[i];
-            totalTAT += tat[i];
-        }
-
-        System.out.println("Process\tBT\tAT\tWT\tTAT\tCT");
-        for (int i = 0; i < n; i++) {
-            System.out.println((i+1) + "\t\t" + bt[i] + "\t" + at[i] +
-                               "\t" + wt[i] + "\t" + tat[i] + "\t" + ct[i]);
-        }
-
-        System.out.println("\nAverage Waiting Time = " + (double) totalWT / n);
-        System.out.println("Average Turnaround Time = " + (double) totalTAT / n);
-    }
+public class SJFPreemptive {
 
     public static void main(String[] args) {
 
-        int burst_time[] = {5, 9, 6};
-        int arrival_time[] = {0, 3, 6};
+        Scanner sc = new Scanner(System.in);
 
-        findAvgTime(burst_time, arrival_time);
+        System.out.print("Enter number of processes: ");
+        int n = sc.nextInt();
+
+        int[] AT = new int[n];
+        int[] BT = new int[n];
+        int[] RT = new int[n];
+        int[] CT = new int[n];
+        int[] TAT = new int[n];
+        int[] WT = new int[n];
+
+        // Input
+        System.out.println("\nEnter Arrival Time and Burst Time:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Process " + (i + 1) + ":");
+            System.out.print("Arrival Time: ");
+            AT[i] = sc.nextInt();
+            System.out.print("Burst Time: ");
+            BT[i] = sc.nextInt();
+
+            RT[i] = BT[i];   // Remaining Time = Burst Time
+        }
+
+        int currentTime = 0;
+        int completed = 0;
+
+        while (completed < n) {
+
+            int idx = -1;
+            int minRT = Integer.MAX_VALUE;
+
+            // Find process with smallest remaining time
+            for (int i = 0; i < n; i++) {
+                if (AT[i] <= currentTime && RT[i] > 0) {
+                    if (RT[i] < minRT) {
+                        minRT = RT[i];
+                        idx = i;
+                    }
+                }
+            }
+
+            if (idx == -1) {
+                // CPU idle
+                currentTime++;
+            } else {
+                // Execute for 1 unit time
+                RT[idx]--;
+                currentTime++;
+
+                // If process finished
+                if (RT[idx] == 0) {
+                    completed++;
+                    CT[idx] = currentTime;
+                    TAT[idx] = CT[idx] - AT[idx];
+                    WT[idx] = TAT[idx] - BT[idx];
+                }
+            }
+        }
+
+        // Output
+        System.out.println("\nProcess\tAT\tBT\tCT\tTAT\tWT");
+        for (int i = 0; i < n; i++) {
+            System.out.println("P" + (i + 1) + "\t"
+                    + AT[i] + "\t"
+                    + BT[i] + "\t"
+                    + CT[i] + "\t"
+                    + TAT[i] + "\t"
+                    + WT[i]);
+        }
+
+        float totalWT = 0, totalTAT = 0;
+        for (int i = 0; i < n; i++) {
+            totalWT += WT[i];
+            totalTAT += TAT[i];
+        }
+
+        System.out.println("\nAverage Waiting Time = " + (totalWT / n));
+        System.out.println("Average Turnaround Time = " + (totalTAT / n));
+
+        sc.close();
     }
 }
