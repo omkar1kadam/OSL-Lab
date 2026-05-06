97be175239637f1578317ce7ad7b64298db7c162
Omkar Kadam <omkarakadam2408@gmail.com>
2026-05-06 17:05:10 +0000

    all done

--- Main_Practical/osl/exp1.cpp
+++ Main_Practical/osl/exp1.cpp
@@ -6,5 +6,19 @@
 using namespace std;
 
 int main(){
-    
+    int fd = open("test.txt", O_CREAT | O_WRONLY, 0644);
+
+    const char *msg = "Hello bro, only file system calls here!";
+    write(fd,msg,strlen(msg));
+
+    close(fd);
+
+    fd = open("test.txt", O_RDONLY);
+    cout<<fd;
+
+    char buffer[100];
+    read(fd,buffer,sizeof(buffer));
+    write(1,buffer,100);
+
+    close(fd);
 }
